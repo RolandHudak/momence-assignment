79a864e5c00ef4e45e1ecacdf0bcc1ab414582b8
Roland Hudak <rolo.hudak@gmail.com>
2025-03-07 09:12:55 +0100

    feat(ui): table component

--- src/features/ConversionTool.tsx
+++ src/features/ConversionTool.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchConversionRates } from 'services/api-client';
+import { ConversionRate, fetchConversionRates } from 'services/api-client';
+import { Table, TableColumn } from 'components/Table';
 
 export const ConversionToolFeature = () => {
     const { data } = useQuery({
@@ -7,10 +8,18 @@ export const ConversionToolFeature = () => {
         queryFn: fetchConversionRates,
     });
 
+    const columns: TableColumn<ConversionRate>[] = [
+        { dataIndex: 'country', title: 'Country' },
+        { dataIndex: 'currency', title: 'Currency' },
+        { dataIndex: 'amount', title: 'Amount' },
+        { dataIndex: 'code', title: 'Code' },
+        { dataIndex: 'rate', title: 'Rate' },
+    ];
+
     return (
         <>
             <h1>Currency Exchange App</h1>
-            <pre>{JSON.stringify(data, null, 4)}</pre>
+            {data && <Table data={data} columns={columns} />}
         </>
     );
 };
